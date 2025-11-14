a2ec15584b481ffb4f02a3b8e618d4ff48f0d935
talokin1 <bovan494@gmail.com>
2025-11-14 13:13:37 +0200

    Anal

--- uBKI_parsing/Draft/analyze.py
+++ uBKI_parsing/Draft/analyze.py
@@ -1,8 +1,8 @@
-# 1. Знаходимо клієнтів, які мають більше 1 банку
-bank_counts = summary.groupby("CONTRAGENTAIDENTIFYCODE")["BANKBID"].nunique()
-multi_bank_clients = bank_counts[bank_counts > 1].index
+client_bank_map = (
+    merged[["CONTRAGENTAIDENTIFYCODE", "BANKBID"]]
+    .drop_duplicates()
+    .groupby("CONTRAGENTAIDENTIFYCODE")["BANKBID"]
+    .unique()
+)
 
-# 2. Фільтруємо summary тільки на них
-multi_bank_full = summary[summary["CONTRAGENTAIDENTIFYCODE"].isin(multi_bank_clients)]
-
-multi_bank_full
+client_bank_map
